6c899c0c2b69082f8828289c16704591e5ad6f20
Codexa <tapandutta0301@gmail.com>
2026-07-29 23:44:20 +0530

    Configure Supabase client and update database connection

--- app/supabaseClient.js
+++ app/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://agbphtpxvcrihpfgfwwx.supabase.co/rest/v1/';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnYnBodHB4dmNyaWhwZmdmd3d4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMzY3MTcsImV4cCI6MjEwMDkxMjcxN30.UhKjhmXv6D4ARSt5y4Xj8NiMCq1uJEZbu4WH93VcOeo';
+const supabaseUrl = process.env.https://agbphtpxvcrihpfgfwwx.supabase.co/rest/v1/
+const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnYnBodHB4dmNyaWhwZmdmd3d4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMzY3MTcsImV4cCI6MjEwMDkxMjcxN30.UhKjhmXv6D4ARSt5y4Xj8NiMCq1uJEZbu4WH93VcOeo
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
