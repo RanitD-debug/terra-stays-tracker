029a3794e1945ef2984164d72409087a60cc14f1
Codexa <tapandutta0301@gmail.com>
2026-07-29 23:35:46 +0530

    Update Supabase client configuration

--- app/supabaseClient.js
+++ app/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://agbphtpxvcrihpfgfwwx.supabase.co/rest/v1/';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqcHBienpra2VnbHZ0cHFvaG53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMzI0NzYsImV4cCI6MjA4ODkwODQ3Nn0.fboW37-dvR30MUlKB5ccI-GEGQFoHx9v6gdhxlwWOT0';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnYnBodHB4dmNyaWhwZmdmd3d4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMzY3MTcsImV4cCI6MjEwMDkxMjcxN30.UhKjhmXv6D4ARSt5y4Xj8NiMCq1uJEZbu4WH93VcOeo';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
